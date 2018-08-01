8d50c6762c37cd5cfe08475ab83388502bd7d693
Liu, Jerry X <jerry.x.liu@aptiv.com>
2018-08-01 11:34:14 +0200

    Implemented threading

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
-import socket
+import threading
 import pyautogui
+import time
 
 app = Flask(__name__)
+app.config['THREAD'] = None
 
 
 @app.route('/')
 def index():
-    pyautogui.screenshot().save('static/img/screenshot.png')
     return render_template('index.html')
 
 
@@ -20,9 +21,23 @@ def header(r):
     return r
 
 
+def worker(sleep_time=30, keep_screen_alive=False):
+    while 1:
+        # To keep the screen alive
+        if keep_screen_alive:
+            x, y = pyautogui.position()
+            pyautogui.moveTo(0, 0)
+            pyautogui.moveTo(x, y)
+
+        pyautogui.screenshot().save('static/img/screenshot.png')
+        time.sleep(sleep_time)
+
+
 if __name__ == '__main__':
     """
     Creates an accessible page with a desktop screenshot every few minutes.
     """
-    host = socket.gethostbyname(socket.gethostname())   # Manually set host if this fails.
-    app.run(host=host, port=5050)
+    app.config['THREAD'] = threading.Thread(target=worker)
+    app.config['THREAD'].start()
+
+    app.run(port=5050)
